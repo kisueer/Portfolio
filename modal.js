14400d8b46a607f8503c5a142574ec1f5d83ad35
Kisueer <nonamerogg@gmail.com>
2025-05-13 08:14:13 +0300

    Update modal.js

--- modal.js
+++ modal.js
@@ -1,23 +1,19 @@
-// Funktion zum Öffnen eines Modals mit Animation
 function openModal(modalId, event) {
     const modal = document.getElementById(modalId);
     const modalContent = modal.querySelector('.modal-content');
     const appElement = event.currentTarget.getBoundingClientRect();
 
-    // Setzt die Modal-Position basierend auf dem geklickten Element
     modalContent.style.left = `${appElement.left + window.scrollX}px`;
     modalContent.style.top = `${appElement.top + window.scrollY}px`;
     modalContent.style.width = `${appElement.width}px`;
     modalContent.style.height = `${appElement.height}px`;
 
-    // Erst Modal anzeigen, dann Animation starten
     modal.style.display = 'block';
 
     setTimeout(() => {
         modal.classList.add('show');
         modalContent.classList.add('show');
 
-        // Modal auf volle Größe setzen
         modalContent.style.left = '50%';
         modalContent.style.top = '50%';
         modalContent.style.width = '50%';
@@ -29,7 +25,7 @@ function openModal(modalId, event) {
     document.body.style.overflow = 'hidden';
 }
 
-// Funktion zum Schließen eines Modals mit Animation
+
 function closeModal(modalId) {
     const modal = document.getElementById(modalId);
     const modalContent = modal.querySelector('.modal-content');
@@ -37,15 +33,15 @@ function closeModal(modalId) {
     modal.classList.remove('show');
     modalContent.classList.remove('show');
 
-    // Warte auf die Animation, bevor das Modal ausgeblendet wird
+
     setTimeout(() => {
         modal.style.display = 'none';
-        modalContent.removeAttribute('style'); // Zurücksetzen der Inline-Stile
-        document.body.style.overflow = ''; // Hintergrund-Scroll wieder aktivieren
+        modalContent.removeAttribute('style'); 
+        document.body.style.overflow = '';
     }, 400);
 }
 
-// Schließen des Modals bei Klick außerhalb des Inhalts
+
 window.onclick = function (event) {
     document.querySelectorAll('.modal').forEach((modal) => {
         if (event.target === modal) {
@@ -54,12 +50,12 @@ window.onclick = function (event) {
     });
 };
 
-// Schließen des Modals mit der Escape-Taste
+
 document.addEventListener('keydown', function(event) {
     if (event.key === "Escape") {
         document.querySelectorAll('.modal').forEach(modal => {
             modal.style.display = 'none';
-            document.body.style.overflow = ''; // Hintergrund-Scroll wieder aktivieren
+            document.body.style.overflow = '';
         });
     }
 });
